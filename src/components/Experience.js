2ee2f266024640280ff73cf9ef66810a387fc249
Aymeric <aymeric.bouault@gmail.com>
2022-05-23 08:48:31 +0200

    Experience component

---
+++ src/components/Experience.js
@@ -0,0 +1,89 @@
+import React from "react";
+
+const Experience = () => {
+  return (
+    <div className="experience">
+      <div className="d-flex justify-content-center my-5">
+        <h1>experience</h1>
+      </div>
+      <div className="container experience-wrapper">
+        {/* 1 */}
+        <div className="timeline-block timeline-block-right">
+          <div className="marker"></div>
+          <div className="timeline-content">
+            <h3>2014-2015</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+              officiis aut quibusdam vel perferendis deleniti tenetur ea
+              doloremque! Repellat aut debitis modi culpa commodi. Dolorum
+              libero ut quae mollitia aspernatur recusandae inventore ducimus.
+              Placeat perferendis, explicabo deserunt atque voluptates fugiat?
+            </p>
+          </div>
+        </div>
+
+        {/* 2 */}
+        <div className="timeline-block timeline-block-left">
+          <div className="marker"></div>
+          <div className="timeline-content">
+            <h3>2015-2016</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+              officiis aut quibusdam vel perferendis deleniti tenetur ea
+              doloremque! Repellat aut debitis modi culpa commodi. Dolorum
+              libero ut quae mollitia aspernatur recusandae inventore ducimus.
+              Placeat perferendis, explicabo deserunt atque voluptates fugiat?
+            </p>
+          </div>
+        </div>
+
+        {/* 3 */}
+        <div className="timeline-block timeline-block-right">
+          <div className="marker"></div>
+          <div className="timeline-content">
+            <h3>2016-2017</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+              officiis aut quibusdam vel perferendis deleniti tenetur ea
+              doloremque! Repellat aut debitis modi culpa commodi. Dolorum
+              libero ut quae mollitia aspernatur recusandae inventore ducimus.
+              Placeat perferendis, explicabo deserunt atque voluptates fugiat?
+            </p>
+          </div>
+        </div>
+
+        {/* 4 */}
+        <div className="timeline-block timeline-block-left">
+          <div className="marker"></div>
+          <div className="timeline-content">
+            <h3>2017-2018</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+              officiis aut quibusdam vel perferendis deleniti tenetur ea
+              doloremque! Repellat aut debitis modi culpa commodi. Dolorum
+              libero ut quae mollitia aspernatur recusandae inventore ducimus.
+              Placeat perferendis, explicabo deserunt atque voluptates fugiat?
+            </p>
+          </div>
+        </div>
+
+        {/* 5 */}
+        <div className="timeline-block timeline-block-right">
+          <div className="marker"></div>
+          <div className="timeline-content">
+            <h3>2018-2020</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+              officiis aut quibusdam vel perferendis deleniti tenetur ea
+              doloremque! Repellat aut debitis modi culpa commodi. Dolorum
+              libero ut quae mollitia aspernatur recusandae inventore ducimus.
+              Placeat perferendis, explicabo deserunt atque voluptates fugiat?
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
--- src/components/Services.js
+++ src/components/Services.js
@@ -11,7 +11,7 @@ const Services = () => {
       <h1 className="py-5">my services</h1>
       <div className="container">
         <div className="row">
-          <div className="col-md-3 col-sm-6">
+          <div className="col-lg-3 col-md-6 col-sm-6">
             <div className="box">
               <div className="circle">
                 <FontAwesomeIcon className="icon" icon={faDesktop} size="2x" />
@@ -23,7 +23,7 @@ const Services = () => {
               </p>
             </div>
           </div>
-          <div className="col-md-3 col-sm-6">
+          <div className="col-lg-3 col-md-6 col-sm-6">
             <div className="box">
               <div className="circle">
                 <FontAwesomeIcon className="icon" icon={faFileCode} size="2x" />
@@ -32,7 +32,7 @@ const Services = () => {
               <p>Your website will be build with an new proven technologies.</p>
             </div>
           </div>
-          <div className="col-md-3 col-sm-6">
+          <div className="col-lg-3 col-md-6 col-sm-6">
             <div className="box">
               <div className="circle">
                 <FontAwesomeIcon
@@ -48,7 +48,7 @@ const Services = () => {
               </p>
             </div>
           </div>
-          <div className="col-md-3 col-sm-6">
+          <div className="col-lg-3 col-md-6 col-sm-6">
             <div className="box">
               <div className="circle">
                 <FontAwesomeIcon className="icon" icon={faGoogle} size="2x" />
