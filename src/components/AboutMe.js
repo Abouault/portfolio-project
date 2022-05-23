1b52620b343a811b31e224378a6c35328862dd3e
Aymeric <aymeric.bouault@gmail.com>
2022-05-23 06:41:20 +0200

    About me

---
+++ src/components/AboutMe.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const AboutMe = () => {
+  return (
+    <div className="container py-5">
+      <div className="row">
+        <div className="col-lg-6 col-xm-12">
+          <div className="photo-wrap mb-5">
+            <img
+              className="profile-img"
+              src="https://avatars.githubusercontent.com/u/65455508?v=4"
+              alt="author"
+            />
+          </div>
+        </div>
+        <div className="col-lg-6 col-xm-12">
+          <h1 className="about-heading">about me</h1>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutMe;
