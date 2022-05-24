3449d80f5fa257b306f71b184a43376d0ae8eb3e
Aymeric <aymeric.bouault@gmail.com>
2022-05-24 03:31:10 +0200

    deploy app

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div id="home" className="header-wrapper">
       <div className="main-info">
-        <h1>web development and websites promotions</h1>
+        <h1>web development and websites promotions !!</h1>
         <Typed
           className="typed-text"
           strings={[
